e3dab8316377885d62948b957c78a8b4ec17e7e2
kagreene <kristengreene2023@U.northwestern.edu>
2024-12-24 11:49:17 -0500

    completed project & resume sections

--- src/sections/AboutMe.jsx
+++ src/sections/AboutMe.jsx
@@ -5,6 +5,7 @@ export default function AboutMe() {
     return (
         <>
             <Header title="About Me" />
+            {/*Need to add recent photo or avatar and a short bio */}
             <Footer />
         </>
     );
--- src/sections/Portfolio.jsx
+++ src/sections/Portfolio.jsx
@@ -1,10 +1,15 @@
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-
+import Project from '../components/Project';
 export default function Portfolio() {
     return (
         <>
             <Header title="Portfolio" />
+            {/* Group project 1
+            if we can do non-deployed apps, include 
+            vehicle-builder, employee-tracker,
+            can include readme generator, MAYBE weather-dashboard, and week 1 project if there is nothing better
+             */}
             <Footer />
         </>
     );
